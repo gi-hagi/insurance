1285519e07140a351ebf70d57172715602301cec
gi-hagi <yuto.0308bsk@gmail.com>
2023-04-17 20:50:25 +0900

    itemの一覧表示と新規投稿画面の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :concept, :feature).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :concept, :feature, :merit, :demerit).merge(user_id: current_user.id)
   end
 end
