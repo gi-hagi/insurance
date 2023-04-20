bd4f2a3fe760ae11e72ee0fe02ff4d2cf30ba704
gi-hagi <yuto.0308bsk@gmail.com>
2023-04-20 14:43:27 +0900

    編集削除機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :item_action, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :log_edit, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all
@@ -21,7 +23,22 @@ class ItemsController < ApplicationController
 
   def show
   end
-  
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_action
@@ -30,5 +47,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :concept, :feature, :merit, :demerit).merge(user_id: current_user.id)
+  end
+
+  def log_edit
+    redirect_to root_path unless current_user == @item.user 
   end
 end
