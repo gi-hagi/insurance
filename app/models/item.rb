22cd5fd9b45f629ed085ba7692457cdc9b604ed4
gi-hagi <yuto.0308bsk@gmail.com>
2023-05-17 19:57:25 +0900

    お気に入り登録ボタンの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,8 @@ class Item < ApplicationRecord
   validates :title, presence: true
   validates :concept, presence: true
   validates :feature, presence: true
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
