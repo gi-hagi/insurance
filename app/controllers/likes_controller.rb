87153560259d2c9c4ad736cb35177860476bb55c
gi-hagi <yuto.0308bsk@gmail.com>
2023-05-21 21:51:45 +0900

    お気に入り登録機能のif分

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,15 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @item = Item.find(params[:item_id])
     like = current_user.likes.build(item_id: params[:item_id])
     like.save
+    redirect_to root_path
   end
 
 
   def destroy
-    @item = Item.find(params[:item_id])
     like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     like.destroy
+    redirect_to root_path
   end
 end
