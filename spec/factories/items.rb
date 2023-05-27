3b9d581fca462f3242af6e45fad31d032192b39a
gi-hagi <yuto.0308bsk@gmail.com>
2023-05-27 13:55:05 +0900

    itemのテスト実行

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :item do
-    
+    title {Faker::Lorem.sentence}
+    concept {Faker::Lorem.sentence}
+    feature {Faker::Lorem.sentence}
+    merit {Faker::Lorem.sentence}
+    demerit {Faker::Lorem.sentence}
+    association :user
   end
 end
